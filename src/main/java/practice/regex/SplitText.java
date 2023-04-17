fd98e340f9a35d7d12c2227b9055f3db522012a3
Olga Malkova <Vodeviiil@mail.ru>
2023-04-17 19:38:19 +0300

    + Практическое задание 9.9

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -5,10 +5,31 @@ public class SplitText {
   public static void main(String[] args) {
 
   }
-
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+    String textAnd = "";
 
+    for (int i = 0; i < text.length(); i++) {
+      if (text.length() == -1) {
+        break;
+      }
+      if (Character.isDigit(text.charAt(i))) {
+        continue;
+      }
+      if ((text.charAt(i) != ' ') && (!(Character.isLetter(text.charAt(i)))))  {
+        continue;
+      }
+      if (text.charAt(i) != ' ')  {
+        textAnd = textAnd + text.charAt(i);
+      }
+      if ((text.charAt(i) == ' ') && (Character.isLetter(text.charAt(i + 1)))) {
+        textAnd = textAnd  + "\n";
+      }
+    }
+    return textAnd;
+  }
 }
+
+
+//Реализуйте метод, который принимает в качестве аргумента строку, содержащую английский текст (не менее 100 слов).
+// Метод должен разделить текст на слова и вернуть строку, состоящую из полученных слов, где каждое слово будет на отдельной строке.
+// Знаки препинания и числа не являются словами.
--- src/main/java/practice/string/SequentialWordsNumbers.java
+++ src/main/java/practice/string/SequentialWordsNumbers.java
@@ -1,8 +1,27 @@
 package practice.string;
+import java.util.Scanner;
 
 public class SequentialWordsNumbers {
-
     public static String sequentialWordsNumbers(String text){
-        return "";
+
+        String textAnd = "";
+        int sum = 1;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.length() == -1) {
+                break;
+            }
+            if ((i == 0) && (text.charAt(i) != ' ')) {
+                textAnd = textAnd + "(" + sum + ")" + " ";
+            }
+            if (text.charAt(i) != ' ') {
+                textAnd = textAnd + text.charAt(i);
+            }
+            if  (text.charAt(i) == ' ') {
+                sum++;
+                textAnd = textAnd + text.charAt(i)+ "(" + sum + ")" + " ";
+            }
+        }
+        return textAnd;
     }
 }
